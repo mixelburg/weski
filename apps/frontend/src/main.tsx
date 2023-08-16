22ab5921e2d0a405b5f2f8a4e463a5972bd43f48
Ivan Krokha <ivan.k@track160.com>
2023-08-16 13:52:11 +0300

    stable

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -3,16 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'jotai'
 import 'react-toastify/dist/ReactToastify.css'
-import App from '@/app/app';
-
+import App from '@/app/app'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-  <Provider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
-  </React.StrictMode>
+    <Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 )
-
